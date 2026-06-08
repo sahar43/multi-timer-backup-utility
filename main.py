3c1d810e8b3e0f907be29510a61fc0001c47afcb
Sahar <sahar.frank@gmail.com>
2026-06-08 17:31:09 +0100

    Finished

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ with open(backups_settings_file, 'r') as f:
    backups_path = data['backups_path']
    to_backup_path = data['to_backup_path']
 
-backups_data_path = backups_path + '/' + backups_data_filename
+backups_data_path = os.path.join(backups_path, backups_data_filename)
 
 
 def getMinute():
@@ -63,7 +63,7 @@ def readBackupsData():
    with open(backups_data_path, 'r') as f:
       return json.load(f)
 
-def writeBackupsData(timers_last_minute, timers_backups_ordered):
+def writeBackupsDataExtended(timers_last_minute, timers_backups_ordered):
    data = {
       'timers_last_minute': timers_last_minute,
       'timers_backups_ordered': timers_backups_ordered
@@ -79,17 +79,15 @@ def writeBackupsData(data):
 
 
 def initialiseBackupsData():
-   minute = getMinute()
-
    timers_last_minute = {}
    timers_backups_ordered = {}
    
    for timer in timers_settings:
       name = timer['name']
-      timers_last_minute[name] = minute
+      timers_last_minute[name] = 0 # set it to 0 be default to make the longest backups first
       timers_backups_ordered[name] = []
 
-   writeBackupsData(timers_last_minute, timers_backups_ordered)
+   writeBackupsDataExtended(timers_last_minute, timers_backups_ordered)
 
    backup(lowest_timer)
 
@@ -97,57 +95,82 @@ def initialiseBackupsData():
 def backup(timer):
    def createBackup(timer, data):
       # create the lz4 backup file
-
       filename = getDateStr() + " " + timer['name'] + ".tar.lz4"
       
-      with open(backups_path + "/" + filename, mode='wb') as file: # get the file
+      with open(os.path.join(backups_path, filename), mode='wb') as file: # get the file
          with lz4.frame.open(file, mode='wb') as lz4_file: # get the lz4 frame file
             with tarfile.open(fileobj=lz4_file, mode='w|') as tar: # open the lz4 frame file with streaming mode
                tar.add(to_backup_path, arcname=os.path.basename(to_backup_path)) # arcname ensures that the archive doesn't contain the full absolute path
       
+      # add the newest minute
+      data['timers_last_minute'][timer['name']] = getMinute()
+
       # add the backup file to the list
+      data['timers_backups_ordered'][timer['name']].append(filename)
 
 
-
-   def deleteLastBackup(data):
-      def delete():
-         pass ## TODO
-
-      pass ## TODO
-   
    def countBackups(timer, data):
       return len(data['timers_backups_ordered'][timer['name']])
+
+
+   def deleteOldestBackup(data):
+      def delete(filepath):
+         os.remove(filepath)
+      
+      delete(os.path.join(backups_path, data['timers_backups_ordered'][timer['name']][0]))
+
+      data['timers_backups_ordered'][timer['name']].pop(0)
 
 
    data = readBackupsData()
 
    createBackup(timer, data)
 
-   if (countBackups(timer, data) > timer['keep_amount']):
-      deleteLastBackup()
+   while (countBackups(timer, data) > timer['keep_amount']):
+      deleteOldestBackup(data)
    
    writeBackupsData(data)
-   
-   print(countBackups(timer, data)) # TODO REMOVE
 
 
 def clock():
-   while True:
-      time.sleep(lowest_timer_minutes*60)
+   def chooseTimer():
+      minute = getMinute()
+      data = readBackupsData()
 
-      ## TODO: check and trigger backup
+      def getTimerMinutes(timer):
+         return timer['timer_minutes']
+
+      # get the elapsed minutes since the latest backup of that timer
+      def getLatestElapsed(timer):
+         return minute - data['timers_last_minute'][timer['name']]
+
+      # get the timers that can be chosen (elapsed time high enough)
+      valids = [timer for timer in timers_settings if getLatestElapsed(timer) > getTimerMinutes(timer)]
+
+      # choose the one with the highest timer minutes
+      if (valids and len(valids) > 0):
+         chosen = None
+
+         for timer in valids:
+            if not chosen:
+               chosen = timer
+            else:
+               if getTimerMinutes(timer) > getTimerMinutes(chosen):
+                  chosen = timer
+         
+         return chosen
+
+   while True:
+      timer = chooseTimer()
+
+      if timer:
+         backup(timer)
+
+      time.sleep(lowest_timer_minutes*60) # TODO: move to front
 
 
 # initialise the backups data file if it doesnt exist 
 if not os.path.isfile(backups_data_path):
    initialiseBackupsData()
 
-#clock() # TODO ENABLE
-
-
-
-#backup(lowest_timer)
-
-from pathlib import Path
-
-Path.rmdir(to_backup_path)
+clock()
